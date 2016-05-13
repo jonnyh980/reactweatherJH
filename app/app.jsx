8baf52c72c15171f7e534738df623720e3a85b03
Jonathan Harrocks <jonnyh980@hotmail.com>
2016-05-13 22:31:26 +0100

    added loads of packages

--- app/app.jsx
+++ app/app.jsx
@@ -1,10 +1,15 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
-var Weather = require('Weather');
-var About = require('About');
-var Examples = require('Examples');
+var React = require("react");
+var ReactDOM = require("react-dom");
+var {Route, Router, IndexRoute, hashHistory} = require("react-router");
+var Main = require("Main");
+var Weather = require("Weather");
+var About = require("About");
+var Examples = require("Examples");
+
+// load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 
 ReactDOM.render(
   <Router history={hashHistory}>
@@ -14,5 +19,5 @@ ReactDOM.render(
       <IndexRoute component={Weather}/>
     </Route>
   </Router>,
-  document.getElementById('app')
+  document.getElementById("app")
 );
